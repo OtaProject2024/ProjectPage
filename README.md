a7ea825eff52ed8a9ad94e7cb2ef392d29acbacf
ryokpen87044 <106538588+ryokpen87044@users.noreply.github.com>
2024-05-24 17:30:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# OtaProject 2024
+
+Senshu University School of Network and Information - PROJECT 2024
+
+## Overview
+
+We created a website using Vue.js to showcase OtaProject 2024. The site features a background displaying 3D models created by project members.
+The implementation of an SPA web page has enhanced user experience by reducing wait times associated with page transitions. By adopting SPA principles, data is asynchronously fetched from the server after the initial page load, facilitating dynamic content updates without full page reloads.
+
+## Technical Overview
+
+### Programming Language
+
+- JavaScript
+- Node.js (v20.12.2)
+- Vite (v5.1.0)
+
+### Frameworks/Libraries
+
+- Vue.js (v3.4.15)
+  - Vue Router (v4.2.5)
+- Three.js (r161)
+- Tailwind CSS (v3.4.1)
+
+### Deployment
+
+Build with vite and published on Senshu University server.
+
+### License
+
+The default copyright laws apply, meaning that we retain all rights to source code and no one may reproduce, distribute, or create derivative works from ours work.
---
+++ src/components/modules/active/right.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from "vue";
+
+const type = defineProps({
+  month: String,
+  contents: Array,
+  imgs: Array,
+});
+
+let flg = ref(false);
+const over = () => {
+  flg.value = true;
+};
+const leave = () => {
+  flg.value = false;
+};
+</script>
+
+<template>
+  <div class="flex md:contents" @mouseover="over" @mouseleave="leave">
+    <div class="animate-vertical-in relative col-start-5 col-end-6 mr-10 md:mx-auto">
+      <div class="h-full w-6 flex items-center justify-center">
+        <div class="h-full w-0.5 pointer-events-none bg-white"></div>
+      </div>
+      <div class="absolute h-4 w-4 rounded-full ml-1 bg-white top-[21%] md:top-[22%]"></div>
+    </div>
+    <div class="animate-month-right-in hov-hl rounded-lg col-start-6 col-end-10 p-4 my-4 mr-auto">
+      <h2 class="text-4xl md:text-5xl">{{ month }}</h2>
+      <ul class="list-disc list-inside pl-2 mt-3">
+        <li v-for="content in contents">{{ content }}</li>
+      </ul>
+      <transition
+        enter-from-class="opacity-0"
+        enter-active-class="duration-200"
+        leave-to-class="opacity-0"
+        leave-active-class="duration-200"
+      >
+        <ul class="list-none list-inside grid justify-items-center pl-2 mt-3" v-show="flg">
+          <li v-for="img in imgs"><img class="w-72" :src="img" alt="img" /></li>
+        </ul>
+      </transition>
+    </div>
+  </div>
+</template>
