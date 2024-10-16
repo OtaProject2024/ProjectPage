5bbe6231cef3dc01379e3404ca440b3a9cd89d25
ryokpen87044 <106538588+ryokpen87044@users.noreply.github.com>
2024-10-16 12:13:28 +0900

    Fix member page.

--- src/components/modules/member/profile.vue
+++ src/components/modules/member/profile.vue
@@ -8,8 +8,8 @@ const type = defineProps({
 </script>
 
 <template>
-  <div class="hov-hl rounded-lg py-2 mt-10 grid justify-items-center">
-    <img :src="path" alt="icon" class="animate-icon-in size-52" />
+  <div class="hov-hl rounded-lg pt-5 pb-3 mt-10 grid justify-items-center">
+    <img :src="path" alt="icon" class="animate-icon-in size-52 rounded-full" />
     <div class="animate-profile-in text-center">
       <p class="font-light text-base">{{ course }}</p>
       <h2 class="font-bold text-3xl">{{ name }}</h2>
