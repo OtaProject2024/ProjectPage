8a41841c8b89f7ebf96965cb98847a640b6526dc
ryokpen87044 <106538588+ryokpen87044@users.noreply.github.com>
2024-11-03 07:43:21 +0900

    Fix home page.

--- src/components/pages/home.vue
+++ src/components/pages/home.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="absolute h-screen z-5 overflow-y-scroll overscroll-none">
+  <div class="absolute h-screen w-screen z-5 overflow-y-scroll overscroll-none">
     <div class="mt-52 pl-3 sm:pl-6 md:mt-72 md:pl-12 lg:pl-24 xl:pl-36">
       <div class="flex flex-col font-Poppins text-white">
         <div class="animate-title-in">
@@ -10,6 +10,7 @@
         <div class="animate-title-in mt-10">
           <p class="font-light text-2xl md:text-3xl">Indoor Fishing - Realistic experience with fish robot.</p>
         </div>
+        <div class="h-screen"></div>
       </div>
     </div>
   </div>
