678eee71b6adf5fea4d3f5f9602e1ecf8278c53e
ryokpen87044 <106538588+ryokpen87044@users.noreply.github.com>
2024-12-14 15:49:20 +0900

    Fix contents page.

--- src/components/modules/contents/video.vue
+++ src/components/modules/contents/video.vue
@@ -7,11 +7,14 @@ const videoPush = () => event("play Video");
 
 <template>
   <div class="animate-part-in text-gray-300 w-11/12 flex flex-col items-center p-3 mt-2 mb-12">
+    <!--    <p>-->
+    <!--      プロジェクトの活動を紹介する動画です。-->
+    <!--    </p>-->
+    <!--    <p>-->
+    <!--      <span class="font-semibold text-gray-100">音声が出ますので、音量にご注意ください。</span>-->
+    <!--    </p>-->
     <p>
-      プロジェクトの活動を紹介する動画です。
-    </p>
-    <p>
-      <span class="font-semibold text-gray-100">音声が出ますので、音量にご注意ください。</span>
+      coming soon...
     </p>
     <video class="w-full rounded-lg my-5"
            controls controlsList="nodownload" src="#" @click="videoPush"></video>
